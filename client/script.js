dd11777635e33b7870668acf59d2202823bbd731
amvinai <129261726+amvinai@users.noreply.github.com>
2023-03-29 23:08:17 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chatgptamvinai.onrender.com', {
+    const response = await fetch('https://amvin-gpt-ai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
